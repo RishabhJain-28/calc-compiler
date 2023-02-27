7941ff6d8b4a60b3fc9accfd6851cd10dc7833d8
Rishabh Jain <rishabhnjain@gmail.com>
2023-02-28 00:41:08 +0530

    parser

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+mod parser;
+
+fn main() {
+    let mut args = std::env::args();
+    args.next();
+    // let current_path = args.next().unwrap();
+    let source_path = args.next();
+
+    if source_path.is_none() {
+        return eprintln!("Need one argument : <source>.calc file");
+    }
+    proceess_file(&source_path.unwrap());
+}
+
+fn proceess_file(source_path: &str) {
+    const CALC_PREFIX: &str = ".calc";
+
+    if !source_path.ends_with(CALC_PREFIX) {
+        return eprintln!(
+            "Invalid argument {}, file must end with {}",
+            source_path, CALC_PREFIX
+        );
+    }
+    let source_code = std::fs::read_to_string(source_path);
+    if source_code.is_err() {
+        return eprintln!(
+            "Error reading file {}\n {}",
+            source_path,
+            source_code.unwrap_err()
+        );
+    }
+    let source_code = source_code.unwrap();
+    let parsed_program;
+    match parser::parse_program(&source_code) {
+        Ok((rest, syntax_tree)) => {
+            let trimmed_rest = rest.trim();
+            if trimmed_rest.len() > 0 {
+                eprintln!(
+                    "Invalid remaining code in '{}': {}",
+                    source_path, trimmed_rest
+                );
+                return;
+            }
+            parsed_program = syntax_tree;
+        }
+        Err(err) => {
+            eprintln!("Invalid code in '{}': {:?}", source_path, err);
+            return;
+        }
+    }
+
+    println!("Parsed program: {:#?}", parsed_program);
+}
